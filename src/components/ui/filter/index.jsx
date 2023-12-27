6d927f8d38d5aca1c2f056b6e3bc368355ec9258
abduganii <abduganiabdu2@gmail.com>
2023-12-27 15:10:27 +0500

    fixed

--- src/components/ui/filter/index.jsx
+++ src/components/ui/filter/index.jsx
@@ -1,68 +1,100 @@
-import { useRef, useState } from 'react'
-import BlueBtn from '../button/blue-buttun'
-import DropDawn from '../dropdawn'
-import { Checked, DawnIcan, DeleteIcon, PlusIcon } from '../icons'
-import { statusOder } from './data'
-import cls from './filter.module.scss'
+import { useRef, useState } from "react";
+import BlueBtn from "../button/blue-buttun";
+import DropDawn from "../dropdawn";
+import { Checked, DawnIcan, DeleteIcon, PlusIcon } from "../icons";
+import { statusOder } from "./data";
+import cls from "./filter.module.scss";
 
-export default function Filter({ SelectAll, deleteAll, Create, page, ...other }) {
-    const [openStatus, setOpenSatus] = useState(false)
+export default function Filter({
+  SelectAll,
+  deleteAll,
+  Create,
+  page,
+  ...other
+}) {
+  console.log(page);
+  const [openStatus, setOpenSatus] = useState(false);
 
-    const [bac, setbac] = useState(false)
-    const back = useRef()
-    return (
-        <div className={cls.Filter}>
-            <div className={cls.Filter__filters}>
-                <input className={`${cls.Filter__filters__input} ${cls.Filter__filters__search}`} type="text" placeholder='Имя' />
-                <div
-                    className={`${cls.Filter__filters__input} ${cls.Filter__filters__select}`}
-                    onClick={() => {
-                        setbac(true)
-                        setOpenSatus(true)
-                    }}
-                >
-                    Статус заказа
-                    <DawnIcan />
-                    {
-                        openStatus && <DropDawn
-                            array={statusOder}
-                            onClick={() => {
-                                setbac(false)
-                                setOpenSatus(false)
-                            }
-                            }
-                            style={{ top: "44px", left: "0px" }}
-                        />
-                    }
-                </div>
+  const [bac, setbac] = useState(false);
+  const back = useRef();
+  return (
+    <div className={cls.Filter}>
+      <div className={cls.Filter__filters}>
+        <input
+          className={`${cls.Filter__filters__input} ${cls.Filter__filters__search}`}
+          type="text"
+          placeholder={page == "order" || page == "client" ? "Имя" : "Название"}
+        />
 
-            </div>
+        {page == "order" && (
+          <div
+            className={`${cls.Filter__filters__input} ${cls.Filter__filters__select}`}
+            onClick={() => {
+              setbac(true);
+              setOpenSatus(true);
+            }}
+          >
+            Статус заказа
+            <DawnIcan />
+            {openStatus && (
+              <DropDawn
+                array={statusOder}
+                onClick={() => {
+                  setbac(false);
+                  setOpenSatus(false);
+                }}
+                style={{ top: "44px", left: "0px" }}
+              />
+            )}
+          </div>
+        )}
+        {page == "client" && (
+          <input
+            className={`${cls.Filter__filters__input} ${cls.Filter__filters__tel}`}
+            type="text"
+            placeholder={"+998"}
+          />
+        )}
+        {(page == "colorWall" ||
+          page == "moreService" ||
+          page == "detailService") && (
+          <input
+            className={`${cls.Filter__filters__input} ${cls.Filter__filters__ark}`}
+            type="text"
+            placeholder={"Артикул"}
+          />
+        )}
+      </div>
 
-            <div className={cls.Filter__form}>
-                <div className={cls.Filter__form__all} onClick={SelectAll}>
-                    <Checked fill={"#484038"} />
-                    <p>Выбрать все</p>
-                </div>
-                <div className={cls.Filter__form__all} onClick={deleteAll}>
-                    <DeleteIcon fill={"#484038"} />
-                    <p>Удалить</p>
-                </div>
-                {
-                    page != "order" ? <BlueBtn style={{ gap: "5px", maxWidth: "151px" }}>
-                        <PlusIcon fill={"white"} />Добавить
-                    </BlueBtn> : ""
-                }
-
-            </div>
-
-            <div ref={back}
-                onClick={(e) => {
-                    if (e.target == back.current) {
-                        setbac(false)
-                        setOpenSatus(false)
-                    }
-                }} className={`${cls.backruond} ${bac ? "displayBlock" : "displayNone"}`}
-            ></div>
+      <div className={cls.Filter__form}>
+        <div className={cls.Filter__form__all} onClick={SelectAll}>
+          <Checked fill={"#484038"} />
+          <p>Выбрать все</p>
         </div>
-    )
+        <div className={cls.Filter__form__all} onClick={deleteAll}>
+          <DeleteIcon fill={"#484038"} />
+          <p>Удалить</p>
+        </div>
+        {page != "order" && page != "client" ? (
+          <BlueBtn style={{ gap: "5px", maxWidth: "151px" }}>
+            <PlusIcon fill={"white"} />
+            Добавить
+          </BlueBtn>
+        ) : (
+          ""
+        )}
+      </div>
+
+      <div
+        ref={back}
+        onClick={(e) => {
+          if (e.target == back.current) {
+            setbac(false);
+            setOpenSatus(false);
+          }
+        }}
+        className={`${cls.backruond} ${bac ? "displayBlock" : "displayNone"}`}
+      ></div>
+    </div>
+  );
 }
--- src/components/ui/item-list/index.jsx
+++ src/components/ui/item-list/index.jsx
@@ -1,36 +1,50 @@
-import StatusBtn from '../button/status-btn'
-import CheckBox from '../form/checkBox'
-import cls from './itemList.module.scss'
+import StatusBtn from "../button/status-btn";
+import CheckBox from "../form/checkBox";
+import cls from "./itemList.module.scss";
 
-export default function ListItem({ id, img, name, time, tel, details = [], price, status, statusChange, file, show, update, remove }) {
+export default function ListItem({
+  id,
+  img,
+  name,
+  time,
+  tel,
+  details = [],
+  price,
+  status,
+  statusChange,
+  file,
+  show,
+  update,
+  remove,
+}) {
+  return (
+    <li className={`${cls.ListItem} ${img && cls.ListItem__withImg} `}>
+      <CheckBox />
+      {img ? (
+        <div className={cls.ListItem__img}>
+          {img == "empty" ? (
+            <img src="/img.svg" width={60} height={70} alt="img" />
+          ) : (
+            <img src={img} width={60} height={70} alt="img" />
+          )}
+        </div>
+      ) : (
+        ""
+      )}
+      {id && <p className={cls.ListItem__detail}>{id}</p>}
+      {time && <p className={cls.ListItem__detail}>{time}</p>}
+      {name && <p className={cls.ListItem__name}>{name}</p>}
+      {tel && <p className={cls.ListItem__tel}>{tel}</p>}
 
-    return (
-        <li className={`${cls.ListItem} ${img && cls.ListItem__withImg} `}>
-            <CheckBox />
-            {img ?
-                <div className={cls.ListItem__img}>
-                    {
-                        img == "empty" ?
-                            <img src='/img.png' width={60} height={70} alt='img' /> :
-                            <img src={img} width={60} height={70} alt="img" />
-                    }
-                </div>
-                : ""}
-            {id && <p className={cls.ListItem__detail}>{id}</p>}
-            {time && <p className={cls.ListItem__detail}>{time}</p>}
-            {name && <p className={cls.ListItem__name}>{name}</p>}
-            {tel && <p className={cls.ListItem__tel}>{tel}</p>}
-
-            {
-                details && details?.map((e, i) => (
-                    <p key={i} className={cls.ListItem__detail}>{e}</p>
-                ))
-            }
-            {price && <p className={cls.ListItem__price}>{price}</p>}
-            {status && <StatusBtn label={status} onChange={statusChange} />}
-            <div className={cls.ListItem__action}>
-                action
-            </div>
-        </li>
-    )
+      {details &&
+        details?.map((e, i) => (
+          <p key={i} className={cls.ListItem__detail}>
+            {e}
+          </p>
+        ))}
+      {price && <p className={cls.ListItem__price}>{price}</p>}
+      {status && <StatusBtn label={status} onChange={statusChange} />}
+      <div className={cls.ListItem__action}>action</div>
+    </li>
+  );
 }
